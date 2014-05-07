82f3e40ef54557ef7c9f108bb8ae45967b636fab
Leo Shin <leo@sh1n.com>
2014-05-07 15:18:17 -0700

    First check in with a ChargeBeeServiceProvider in laravel 4

---
+++ src/Lshin/ChargebeeLaravel4/ChargebeeLaravel4ServiceProvider.php
@@ -0,0 +1,50 @@
+<?php namespace Lshin\ChargebeeLaravel4;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
+use \Chargebee_Environment;
+
+class ChargebeeLaravel4ServiceProvider extends ServiceProvider {
+
+  /**
+   * Indicates if loading of the provider is deferred.
+   *
+   * @var bool
+   */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('lshin/chargebee-laravel4', __DIR__.'/../../config');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->add['config']->package('lshin/chargebee-laravel4', __DIR__.'/../../config');
+        Chargebee_Environment::configure(
+          $this->app['config']->get('chargebee-laravel4::config.site', null),
+          $this->app['config']->get('chargebee-laravel4::config.api_key', null)
+        );
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+
+}
---
+++ src/config/config.php
@@ -0,0 +1,13 @@
+<?php
+    return array(
+        /**
+         * the 'site' of the chargebee instance you are connecting to
+         * https://apidocs.chargebee.com/docs/api#api_authentication
+         */
+        'site' => '',
+        /**
+         * API key, you can get from chargebee dashboard.
+         */
+        'api_key' => ''
+    );
+?>
